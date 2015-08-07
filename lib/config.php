286bdae4f472059d65fcd2841382004b57b9c64a
jchck <hey@justinchick.com>
2015-08-07 15:41:53 -0500

    disable admin bar

--- lib/config.php
+++ lib/config.php
@@ -8,3 +8,5 @@ add_theme_support('soil-nav-walker');       // Enable cleaner nav walker from So
 add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
 add_theme_support('soil-nice-search');      // Enable nice search from Soil
 add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Google CDN
+
+add_filter('show_admin_bar', '__return_false');
