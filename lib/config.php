da5c6e798aea364926931d9662f4424009f9737f
jchck <hey@justinchick.com>
2015-08-08 13:35:54 -0500

    define wp_env & dist_dir

--- lib/config.php
+++ lib/config.php
@@ -14,4 +14,17 @@ add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
 add_theme_support('soil-nice-search');      // Enable nice search from Soil
 add_theme_support('soil-jquery-cdn');       // Enable to load jQuery from the Google CDN
 
+/**
+ * Configuration values
+ */
+if (!defined('WP_ENV')) {
+  // Fallback if WP_ENV isn't defined in your WordPress config
+  // Used in lib/assets.php to check for 'development' or 'production'
+  define('WP_ENV', 'production');
+}
+if (!defined('DIST_DIR')) {
+  // Path to the build directory for front-end assets
+  define('DIST_DIR', '/dist/');
+}
+
 add_filter('show_admin_bar', '__return_false');
